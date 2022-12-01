390ca413f3ddb16218227a7062a75795a4e8002a
Sven <svenkuhlmann1@gmx.de>
2022-12-01 20:37:27 +0100

    text home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatRippleModule } from '@angular/material/core';
 import { RouterModule } from '@angular/router';
@@ -8,7 +10,7 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, TranslateModule, MatCardModule, RouterModule, MatRippleModule],
+  imports: [CommonModule, TranslateModule, MatCardModule, RouterModule, MatRippleModule, MatButtonModule, FlexLayoutModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
